69d31de3c7f64367e271dd3640d935c105b432e4
Nikita Petrenko <npetrenko97@gmail.com>
2019-08-02 22:25:40 +0300

    added UB comment

--- bellman/ub_example/ub_example.cpp
+++ bellman/ub_example/ub_example.cpp
@@ -3,7 +3,7 @@
 int main() {
     DiscreteQFuncEst est;
     static_assert(std::is_convertible_v<DiscreteQFuncEst*, ICloneable*>);
-    bool should_be_true = dynamic_cast<ICloneable*>(&est) != 0;
+    bool should_be_true = dynamic_cast<ICloneable*>(&est) != 0; // UBSan complains here
     bool has_failed = !should_be_true;
     return has_failed;
 }
