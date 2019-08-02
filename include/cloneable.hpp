2bc5badfae73808e1bcefed5dbc93ed20931cb68
Nikita Petrenko <npetrenko97@gmail.com>
2019-08-03 00:48:17 +0300

    stripped even more

--- include/cloneable.hpp
+++ include/cloneable.hpp
@@ -5,21 +5,11 @@
 
 class ICloneable {
 public:
-    inline std::unique_ptr<ICloneable> IClone() const {
-        return std::unique_ptr<ICloneable>(ICloneImpl());
-    }
-
     virtual ~ICloneable() = default;
-
-protected:
-    virtual ICloneable* ICloneImpl() const = 0;
 };
 
-template <class... inherit_from>
-struct InheritFrom : public inherit_from... {};
-
 template <class T>
-struct InheritFrom<T> : public T {
+struct InheritFrom : public T {
     using T::T;
 };
 
@@ -27,72 +17,34 @@ template <class Derived, class AnotherBase, bool derived_is_abstract,
           bool base_is_cloneable = std::is_base_of_v<ICloneable, AnotherBase>>
 class _CloneableImpl;
 
-#define __CloneImpl                                                        \
-public:                                                                    \
-    inline std::unique_ptr<Derived> Clone() const {                        \
-        auto ptr = this->ICloneImpl();                                     \
-        return std::unique_ptr<Derived>{static_cast<Derived*>(ptr)};       \
-    }                                                                      \
-                                                                           \
-protected:                                                                 \
-    inline _CloneableImpl* ICloneImpl() const override {                   \
-        if (typeid(*this) != typeid(Derived)) {                            \
-            throw 0;                                        \
-        }                                                                  \
-        auto ret = new Derived(static_cast<const Derived&>(*this));        \
-        return static_cast<_CloneableImpl*>(ret); \
-    }                                                                      \
-                                                                           \
-private:
-
-#define __CloneImplAbstract                                           \
-public:                                                               \
-    inline std::unique_ptr<Derived> Clone() const {                   \
-        auto ptr = this->ICloneImpl();                                \
-        return std::unique_ptr<Derived>{dynamic_cast<Derived*>(ptr)}; \
-    }                                                                 \
-                                                                      \
-private:
-
 // three identical implementations, only the inheritance is different
 
-#define Implement(IsAbstract, ImplType)                                                           \
+#define Implement(IsAbstract)                                                                     \
     /* "no base is defined" case*/                                                                \
     template <class Derived>                                                                      \
     class _CloneableImpl<Derived, void, IsAbstract, false> : public virtual ICloneable {          \
-        ImplType                                                                                  \
     };                                                                                            \
                                                                                                   \
     /* Base is defined, and already provides ICloneable*/                                         \
     template <class Derived, class AnotherBase>                                                   \
     class _CloneableImpl<Derived, AnotherBase, IsAbstract, true> : public AnotherBase {           \
-    public:                                                                                       \
-        using AnotherBase::AnotherBase;                                                           \
-        ImplType                                                                                  \
     };                                                                                            \
                                                                                                   \
     /* Base is defined, but has no ICloneable*/                                                   \
     template <class Derived, class AnotherBase>                                                   \
     class _CloneableImpl<Derived, AnotherBase, IsAbstract, false> : public AnotherBase,           \
                                                                     public virtual ICloneable {   \
-    public:                                                                                       \
-        using AnotherBase::AnotherBase;                                                           \
-        ImplType                                                                                  \
     };
 
-Implement(false, __CloneImpl) 
-Implement(true, __CloneImplAbstract)
+Implement(false) 
+Implement(true)
 
-#undef __CloneImpl
-#undef __CloneImplAbstract
 #undef Implement
 
 template <class Derived, class AnotherBase = void>
 class EnableClone : public _CloneableImpl<Derived, AnotherBase, false> {
-    using _CloneableImpl<Derived, AnotherBase, false>::_CloneableImpl;
 };
 
 template <class Derived, class AnotherBase = void>
 class EnableCloneInterface : public _CloneableImpl<Derived, AnotherBase, true> {
-    using _CloneableImpl<Derived, AnotherBase, true>::_CloneableImpl;
 };
